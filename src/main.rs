7f250930676900b6e0986b59d5fe01456a05cf1b
toku-sa-n <tokusan441@gmail.com>
2020-10-04 14:41:12 +0900

    Create a temporary directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use tempfile::TempDir;
+
+fn main() -> Result<(), std::io::Error> {
+    let tmp_dir = TempDir::new()?;
+
+    println!("Created: {:?}", tmp_dir);
+
+    Ok(())
 }
